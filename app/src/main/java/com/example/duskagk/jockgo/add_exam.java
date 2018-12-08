65636b668d053ea4ea82f93e747724e848acaa25
이씨 <aucun@naver.com>
2018-12-08 15:00:53 +0900

    exam

--- app/src/main/java/com/example/duskagk/jockgo/add_exam.java
+++ app/src/main/java/com/example/duskagk/jockgo/add_exam.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,9 +17,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 public class add_exam extends AppCompatActivity {
+
+    JSONArray jsonArray;
+    String selectSubject;
+    /////
+    int b_no = 10;
+    //////임시
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,27 +39,40 @@ public class add_exam extends AppCompatActivity {
         Button btnSubject = (Button) findViewById(R.id.btnExamSubject);
         Button btnDenouement = (Button) findViewById(R.id.btnExamDenouement);
 
+        getSpinner();
+
         btnSubject.setOnClickListener(clickBtn("subject"));
         btnDenouement.setOnClickListener(clickBtn("denouement"));
     }
 
     private void getSpinner(){
-        NetworkTask networkTask = new NetworkTask("https://che5uuetmi.execute-api.ap-northeast-2.amazonaws.com/test/school", null, "GET");
+
+
+
+        Spinner spinner = (Spinner)findViewById(R.id.examSubject);
+        NetworkTask networkTask = new NetworkTask("https://che5uuetmi.execute-api.ap-northeast-2.amazonaws.com/test/subject?b_no=" + b_no, null, "GET");
         try {
             String result = networkTask.execute().get();
+            Set<String> set = new HashSet<String>();
 
 
 
-            JSONArray jsonArray=new JSONArray(result);
+            jsonArray=new JSONArray(result);
             ArrayAdapter<String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_dropdown_item);
 
             for(int i = 0; i< jsonArray.length(); i++){
                 JSONObject jsonObj = jsonArray.getJSONObject(i);
-                adapter.add(jsonObj.get("u_school").toString());
+                set.add(jsonObj.getString("s_subject"));
             }
 
-            mSchool.setAdapter(adapter);
-            mSchool.setSelection(0);
+            for (Iterator i = set.iterator(); i.hasNext();){
+                adapter.add((String)i.next());
+            }
+
+
+
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener(selectSpinner());
 
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -56,6 +81,37 @@ public class add_exam extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private AdapterView.OnItemSelectedListener selectSpinner(){
+        return new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spinner spinner = (Spinner)findViewById(R.id.examDenouement);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>( getApplicationContext(), android.R.layout.simple_spinner_dropdown_item);
+                selectSubject = (String) parent.getItemAtPosition(position);
+
+                for (int i = 0; i < jsonArray.length(); i++){
+                    try {
+                        JSONObject jsonObj = jsonArray.getJSONObject(i);
+                        if (selectSubject.equals(jsonObj.getString("s_subject"))){
+                            adapter.add(jsonObj.getString("s_denouement"));
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                spinner.setAdapter(adapter);
+                //spinner.setOnItemClickListener(selectSpinner());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        };
     }
 
     private View.OnClickListener clickBtn(final String s) {
@@ -69,7 +125,7 @@ public class add_exam extends AppCompatActivity {
                 EditText editText = (EditText) inflateView.findViewById(R.id.editTextCategory);
                 Button button = (Button)inflateView.findViewById(R.id.btnCategoryPush);
 
-                button.setOnClickListener(clickPush(s));
+                button.setOnClickListener(clickPush(s, editText));
 
 
                 dialog.setView(inflateView);
@@ -79,15 +135,34 @@ public class add_exam extends AppCompatActivity {
         };
     }
 
-    private View.OnClickListener clickPush(final String s){
+    private View.OnClickListener clickPush(final String s, final EditText ed){
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                JSONObject values = new JSONObject();
                 if (s.equals("subject")){
 
-                } else if (s.equals("denouement")){
+                    try {
+                        values.put("b_no", b_no);
+                        values.put("subject", ed.getText().toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
 
+
+
+                } else if (s.equals("denouement")){
+                    try {
+                        values.put("b_no", b_no);
+                        values.put("subject", selectSubject);
+                        values.put("denouement", ed.getText().toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
+
+                NetworkTask networkTask = new NetworkTask("https://che5uuetmi.execute-api.ap-northeast-2.amazonaws.com/test/subject", values, "POST");
+                networkTask.execute();
             }
         };
     }
